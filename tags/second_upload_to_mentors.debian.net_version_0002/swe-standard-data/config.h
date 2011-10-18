da7384034f774ee7d193c4f12c531e050acddc7e
pelliott <pelliott@321e03d4-a0ea-4445-80dd-3d3597bf822b>
2011-10-18 17:26:42 +0000

    state at second upload to mentors.debian.net releases: libswe-1.77.00.0002.tar.bz2 swe-standard-data-00002.tar.bz2
    git-svn-id: svn+ssh://svn.gna.org/svn/swissephauto@137 321e03d4-a0ea-4445-80dd-3d3597bf822b

---
+++ tags/second_upload_to_mentors.debian.net_version_0002/swe-standard-data/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "swe-standard-data"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "swe-standard-data"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "swe-standard-data 00002"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "swe-standard-data"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "00002"
+
+/* Version number of package */
+#define VERSION "00002"
