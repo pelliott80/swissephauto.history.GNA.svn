c80710ea9d3d361a0da27c7fb6a6dbca40006108
pelliott <pelliott@321e03d4-a0ea-4445-80dd-3d3597bf822b>
2015-01-14 05:51:34 +0000

    ./autogen.sh
    git-svn-id: svn+ssh://svn.gna.org/svn/swissephauto@195 321e03d4-a0ea-4445-80dd-3d3597bf822b

--- trunk/swe-extrapolated-data/config.h
+++ trunk/swe-extrapolated-data/config.h
@@ -2,25 +2,25 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Name of package */
-#define PACKAGE "swe-standard-data"
+#define PACKAGE "swe-extrapolated-data"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "swe-standard-data"
+#define PACKAGE_NAME "swe-extrapolated-data"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "swe-standard-data 00005"
+#define PACKAGE_STRING "swe-extrapolated-data 00001"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "swe-standard-data"
+#define PACKAGE_TARNAME "swe-extrapolated-data"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "00005"
+#define PACKAGE_VERSION "00001"
 
 /* Version number of package */
-#define VERSION "00005"
+#define VERSION "00001"
